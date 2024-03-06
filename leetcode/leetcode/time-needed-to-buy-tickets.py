fc0ac47e0d32253055a3374d5a019fe001b85816
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-03-06 19:52:13 +0300

    Add solution for Time Needed to Buy Tickets

---
+++ leetcode/leetcode/time-needed-to-buy-tickets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+
+        temp = tickets[k]
+        ans = 0
+        cnt = 0
+        for i in range(k + 1,len(tickets)):
+            if tickets[i] >= temp:
+                cnt += 1
+
+        for i in tickets:
+            if i < temp:
+                ans += i
+            else:
+                ans += temp
+           
+        return ans - cnt
