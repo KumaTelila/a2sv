d2236204ce4e98885593d8952ed05558122dac8a
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-04-08 17:42:23 +0300

    Created 'B - Mapping the Maze' by A2SV Hub

---
+++ A2SV G5 - Contest #11 08-Apr-2024/B - Mapping the Maze 118499.py
@@ -0,0 +1,47 @@
+# Problem: B - Mapping the Maze - https://codeforces.com/gym/515998/problem/B
+
+from sys import stdin
+from collections import defaultdict, Counter, deque
+import bisect
+import math
+ 
+ 
+input = lambda : stdin.readline().strip()
+ 
+def lint():
+    return list(map(int, input().split()))
+ 
+def intp():
+    return int(input())
+    
+def strp():
+    return input()
+ 
+def lstr():
+    return list(input().split())
+def check(graph, node, m):
+    ans = "unknown topology"
+    l = set()
+    for i in graph:
+        l.add(len(graph[i]))
+    l = list(l)
+    l.sort()
+    if len(l) == 1 and l[0] == 2:
+        return "ring topology"
+    if len(l) == 2 and l[0] == 1 and l[1] == 2:
+        return "bus topology"
+    if len(l) ==  2 and l[0] == 1 and l[1] == m:
+        return  "star topology"
+    return ans   
+def solve():
+    n, m = lint()
+    graph = defaultdict(list)
+    end = -1
+    for _ in range(m):
+        v, e = lint()
+        end = e
+        graph[v].append(e)
+        graph[e].append(v)
+    ans = check(graph, end, m)
+    print(ans)
+solve()
