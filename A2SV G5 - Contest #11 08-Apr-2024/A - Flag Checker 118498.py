f05f3d6cb4befb34efb008932914631094707ae8
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-04-08 17:42:14 +0300

    Created 'A - Flag Checker' by A2SV Hub

---
+++ A2SV G5 - Contest #11 08-Apr-2024/A - Flag Checker 118498.py
@@ -0,0 +1,34 @@
+# Problem: A - Flag Checker - https://codeforces.com/gym/515998/problem/A
+
+from sys import stdin
+from collections import defaultdict, Counter, deque
+import bisect
+import math
+ 
+ 
+input = lambda : stdin.readline().strip()
+ 
+def lint():
+    return list(map(int, input().split()))
+ 
+def intp():
+    return int(input())
+    
+def strp():
+    return input()
+ 
+def lstr():
+    return list(input().split())
+# divmod(a, b)
+def solve():
+    n, m = lint()
+    adj = ''
+    for _ in range(n):
+        s = set(strp())
+        s = list(s)
+        if len(s) > 1 or s[0] == adj:
+            return False
+        adj = s[0]
+    return True 
+a = solve()
+print("YES" if a else "NO")
