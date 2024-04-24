9556cbd82f742feb88cf138029d0f38def7e838a
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-04-24 11:22:07 +0300

    Created 'Redundant Connection' by A2SV Hub

---
+++ 24-Apr-2024/Redundant Connection 131625.py
@@ -0,0 +1,32 @@
+# Problem: Redundant Connection - https://leetcode.com/problems/redundant-connection/
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        root = [i for i in  range(len(edges))]
+        size = [0 for i in range(len(edges))]
+        def find(x):
+            if x == root[x]:
+                return x
+            root[x] = find(root[x])
+            return root[x]
+
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX == rootY:
+                return True
+            if rootX != rootY:
+                if size[rootX] > size[rootY]:
+                    root[rootY] = rootX
+                    size[rootX] += size[rootY]
+                else:
+                    root[rootX] = rootY
+                    size[rootY] += size[rootX]
+        
+        for i, j in edges:
+            if union(i-1, j-1):
+                return [i, j]
+        # return []
+
+        
+        
