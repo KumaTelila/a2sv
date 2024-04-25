29eac34a465e60160190d4c9637db78161149e64
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-04-25 17:51:56 +0300

    Created 'Smallest String With Swaps' by A2SV Hub

---
+++ 25-Apr-2024/Smallest String With Swaps 133305.py
@@ -0,0 +1,42 @@
+# Problem: Smallest String With Swaps - https://leetcode.com/problems/smallest-string-with-swaps/
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.size = [1] * size
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.size[rootX] > self.size[rootY]:
+                self.root[rootY] = rootX
+                self.size[rootX] += self.size[rootY]
+            else:
+                self.root[rootX] = rootY
+                self.size[rootY] += self.size[rootX]
+class Solution:
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        un = UnionFind(len(s))
+        for i, j in pairs:
+            un.union(i, j)
+        ls = [i for i in s]
+        group = defaultdict(list)
+        indexs = defaultdict(list)
+        for i in range(len(s)):
+            parent = un.find(i)
+            group[parent].append(ls[i])
+            indexs[parent].append(i)
+        for i in group:
+            group[i] = sorted(group[i])
+        ans = ['' for i in range(len(s))]
+        for i in group:
+            for j in range(len(group[i])):
+                ind = indexs[i][j]
+                ans[ind] = group[i][j]
+        return ''.join(ans)
