d83eead8c55536f49b8cff0d4cef3f264bc0f038
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-05-01 21:34:54 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 01-May-2024/Find Champion II 138684.py
@@ -0,0 +1,17 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[] for i in range(n)]
+        indegrees = [0 for i in range(n)]
+        for u, v in edges:
+            graph[u].append(v)
+            indegrees[v]+=1
+        queue = deque()
+        for i in range(n):
+            if indegrees[i] == 0:
+                queue.append(i)
+        if len(queue) == 1:
+            return queue[0]
+        return -1
+        
