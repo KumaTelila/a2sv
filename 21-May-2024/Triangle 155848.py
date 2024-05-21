fb763dd7f089553c6bc3247c445607b6a686a23b
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-05-21 15:11:22 +0300

    Created 'Triangle' by A2SV Hub

---
+++ 21-May-2024/Triangle 155848.py
@@ -0,0 +1,11 @@
+# Problem: Triangle - https://leetcode.com/problems/triangle/
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        for i in range(n-2, -1, -1):
+            for j in range(i, -1, -1):
+                triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1])
+        return triangle[0][0]
+
+        
