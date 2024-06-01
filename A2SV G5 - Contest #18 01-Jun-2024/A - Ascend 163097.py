608c6d5660ffe7a3b5d56a173d1f3917f0b7cbae
Kuma Telila <90767545+KumaTelila@users.noreply.github.com>
2024-06-01 21:45:49 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 01-Jun-2024/A - Ascend 163097.py
@@ -0,0 +1,39 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+from sys import stdin
+from collections import defaultdict, Counter, deque
+import bisect
+import math
+ 
+ 
+input = lambda : stdin.readline().strip()
+ 
+def lint():
+    return list(map(int, input().split()))
+ 
+def intp():
+    return int(input())
+    
+def strp():
+    return input()
+ 
+def lstr():
+    return list(input().split())
+# divmod(a, b)
+def solve():
+    n = intp()
+    a = lint()
+    stack = []
+    ans = 1
+    curr = 1
+    for i in range(1, n):
+        if a[i]- a[i-1] > 0:
+            curr += 1
+        else:
+            curr = 1
+        ans= max(curr, ans)
+    
+    print(ans)
+            
+    
+solve()
